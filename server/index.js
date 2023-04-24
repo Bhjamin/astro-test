f06f7afcf1d3674f0dac0b9268cccf4a38d8f5ce
Ben Hancock <benjamin@hancock1.com>
2023-04-24 17:09:23 -0600

    created server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const port = process.env.SERVER_PORT
+
+const express = require('express')
+
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(() => {
+    console.log(`Serving Spencer on port ${port}`)
+})
